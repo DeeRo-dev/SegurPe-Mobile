1755e2df64f508fbefecba2b2d7c51c3905f41fc
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2023-03-04 14:48:03 -0300

    se agrego archivo para el maps

--- App.jsx
+++ App.jsx
@@ -1,27 +1,14 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Home } from './src/components/Home';
 import { MenuLateral } from './src/navigator/MenuLateral';
 
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <Home/> */}
       <MenuLateral/>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor:'blue' 
-   },
-});
