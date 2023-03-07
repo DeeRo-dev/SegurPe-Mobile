b6ef5776c7c36f7f5806178a3621332f609bb698
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2023-03-07 20:47:06 -0300

    Organizando el paginado

--- src/screens/CrearCuenta/CrearCuenta.jsx
+++ src/screens/CrearCuenta/CrearCuenta.jsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { View, Text, Image, TouchableOpacity, } from 'react-native'
 // import { TouchableOpacity } from 'react-native';
 import { Datos1 } from './Datos1';
@@ -10,22 +10,41 @@ import {styles} from './ThemeCrearCuenta';
 
 export const CrearCuenta = () => {
 
-  const [value, setValue] = useState()
+  const [pagina, setPagina] = useState()
+
+
+
+    const paginacion = (data) =>{
+      console.log(data)
+   if (!data) {
+      return <Datos1/>
+   } else {
+    switch (data) {
+      case data === 'data1':
+        return ( <Datos1/>)
+      case data === 'data2':
+        return ( <Datos2/>)
+      case data === 'data3':
+        return ( <Datos3/>)
+     }
+   }
+    }
 
   return (
     <View style={styles.content}>
       <Image style={styles.img} source={require('../../assets/Group169.png')}/>
-       <Datos1/>
-        {/*<Datos2/> 
-       <Datos3/>*/}
+       
+        { 
+          paginacion(pagina)
+        }
         <View style={styles.contentBtnPagination}>
-          <TouchableOpacity  >
+          <TouchableOpacity onPress={() => {setPagina('data1')}} >
             <Text style={styles.textBtnPag}>A</Text>
           </TouchableOpacity> 
-          <TouchableOpacity >
+          <TouchableOpacity onPress={() => {setPagina('data2')}}>
             <Text style={styles.textBtnPag}>B</Text>
           </TouchableOpacity>
-          <TouchableOpacity >
+          <TouchableOpacity onPress={() => {setPagina('data3')}} >
             <Text style={styles.textBtnPag}>C</Text>
           </TouchableOpacity>
         </View>
