981e2027fe1471308254cffc1847a3e3bca85f96
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2023-03-09 21:23:19 -0300

    component perfil, se realizo el avatar

--- src/screens/Perfil/MiPerfil.jsx
+++ src/screens/Perfil/MiPerfil.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {styles} from './ThemeMiPerfil'
+import { Image, StyleSheet, Text, View } from 'react-native';
 export const MiPerfil = () => {
   return (
     <View>
-      <Text >Mi perfil</Text>
+        <View style={styles.contImgAvatar}>
+          <View style={styles.contentAvatar}>
+              <Image source={{uri:"https://www.w3schools.com/howto/img_avatar.png"}}
+                style={styles.avatarPerfil} />
+ 
+         </View>
+        </View>
+     <View>
+     
+     
+    </View>
     </View>
   )
 }
---
+++ src/screens/Perfil/ThemeMiPerfil.jsx
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native'
+
+export const styles = StyleSheet.create({
+    contImgAvatar:{
+        height:260,
+    },
+    contentAvatar:{
+        alignItems:'center',
+        height:150,
+         backgroundColor:'#C5D6FF',
+         borderBottomRadius:130
+         
+    },
+    avatarPerfil:{
+        width:150,
+        height:150,
+        borderRadius:100,
+        marginTop:80
+    },
+})
