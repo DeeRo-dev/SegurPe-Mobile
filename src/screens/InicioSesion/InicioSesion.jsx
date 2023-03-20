e20248d38c113f9b8fdb0fa584703ff8bd707537
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2023-03-20 17:18:05 -0300

    State iniciar sesion

--- src/screens/InicioSesion/InicioSesion.jsx
+++ src/screens/InicioSesion/InicioSesion.jsx
@@ -1,19 +1,24 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react'
+import React, {useState} from 'react'
 import { View, Text, Image, TextInput } from 'react-native'
 import { TouchableOpacity } from 'react-native';
 import {styles} from './ThemeInicioSesion'
 
 export const InicioSesion = () => {
   const navigator = useNavigation()
+  const [email, setEmail] = useState('');
+  const [clave, setClave] = useState('')
+
+  console.log(email, clave)
+
   return (
     <View style={styles.content}>
       
         <View>
           <Text style={styles.titleInput}>Email</Text>
-          <TextInput style={styles.input} placeholder="email"/>
+          <TextInput style={styles.input} placeholder="email" onChangeText={(value) => setEmail(value)}/>
           <Text style={styles.titleInput}>Contraseña</Text>
-          <TextInput style={styles.input} placeholder="Contraseña"/>
+          <TextInput style={styles.input} placeholder="Contraseña" onChangeText={(value) => setClave(value)}/>
         </View>
         <TouchableOpacity  onPress={() => {navigator.navigate('maps')}} style={styles.btn}>
               <Text style={styles.textBtn}>Confirmar</Text>
