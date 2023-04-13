035165fd369d107e4dcbe7ab02eaffb455bf4a26
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2023-04-13 13:31:47 -0300

    menu lateral serenazgo

--- src/navigator/MenuLateral.jsx
+++ src/navigator/MenuLateral.jsx
@@ -1,10 +1,11 @@
+import React, {useState} from "react";
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
 } from "@react-navigation/drawer";
 import { Home } from "../components/Home";
 import { MiPerfil } from "../screens/Perfil/MiPerfil";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Switch, } from "react-native";
 import { Historial } from "../screens/Historial/Historial";
 import { Configuracion } from "../screens/Configuracion/Configuracion";
 import { Map } from "../screens/Map/Map";
@@ -15,17 +16,17 @@ import { ModalBasico } from "../components/Modals/ModalBasico";
 const Drawer = createDrawerNavigator();
 
 const menuItems = [
+  // {
+  //   name: "Estado",
+  //   title: "Estado",
+  //   icon: "Estado",
+  //   component: Historial,
+  // },
   {
     name: "MiPerfil",
     title: "Mí perfil",
     icon: "person-outline",
     component: MiPerfil,
-  },
-  {
-    name: "Historial",
-    title: "Historial de actividad",
-    icon: "time-outline",
-    component: Historial,
   },
   {
     name: "Configuracion",
@@ -52,11 +53,13 @@ export const MenuLateral = () => {
       }}
       drawerContent={(props) => <MenuInterno {...props} />}
     >
+    
       <Drawer.Screen
         name="SesionStackNavigator"
         options={{ title: false }}
         component={SesionStackNavigator}
       />
+    
       {menuItems.map((item) => (
         <Drawer.Screen
           key={item.name}
@@ -71,8 +74,13 @@ export const MenuLateral = () => {
 const MenuInterno = ({ navigation }) => {
   return (
     <DrawerContentScrollView>
+      
       <View style={styles.container}>
         <MenuHeader />
+        <View style={styles.contentItemsEstado}>
+            <Text style={styles.itemTextEstado}><RoundIcon/>  Estado</Text>
+           <ToggleButton/>
+        </View>
         {menuItems.map((item) => (
           <MenuItem
             key={item.name}
@@ -97,7 +105,8 @@ const MenuHeader = () => (
 const MenuItem = ({ title, iconName, onPress }) => (
   <TouchableOpacity onPress={onPress} style={styles.contentItems}>
     <Text style={styles.itemText}>
-      <Ionicons name={iconName} size={20} color="white" /> {title}
+      <Ionicons name={iconName} size={20} color="white" />
+   {title}
     </Text>
   </TouchableOpacity>
 );
@@ -159,4 +168,65 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
     fontSize: 16,
   },
+
+  contentItemsEstado:{
+    flex:1,
+    borderBottomWidth: 1,
+    borderColor: "white",
+    height: 60,
+    justifyContent: "center",
+    borderTopWidth: 2,
+    borderColor: "#FFFFFF",
+    textAlign:'center',
+    flexDirection:'row'
+  },
+  viewDeslizar: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  iconActDes: {
+    flex: 1,
+    width: 10,
+    height: 10,
+    borderRadius: 25
+  },
+  itemTextEstado:{
+    flex: 1,
+    color: "#FFFFFF",
+    fontSize: 16,
+    alignSelf:'center'
+  }
 });
+
+
+const ToggleButton = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const toggleSwitch = () => {
+    setIsEnabled(previousState => !previousState);
+  };
+
+  return (
+    <View style={styles.viewDeslizar}>
+      <Switch
+        trackColor={{ false: "#767577", true: "#4caf50f" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+    </View>
+  );
+}
+
+
+
+const RoundIcon = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  return (
+    <View style={[styles. iconActDes, { backgroundColor: isEnabled ? '#4caf50' : '#f44336' }]} />
+  );
+}
+
+
