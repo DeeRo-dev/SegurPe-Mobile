e4254600c74ced6fa6801c51c675e580749cee6e
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2023-04-25 10:46:29 -0300

    Efecto en la paginacion del onboarding

--- src/screens/InicioDeApp/Home.jsx
+++ src/screens/InicioDeApp/Home.jsx
@@ -14,7 +14,51 @@ export const HomeOnboarding = () => {
 
   const [select, setSelect] = useState(0)
   const ref = useRef();
-
+  const [activeButton, setActiveButton] = useState({
+    btn1:true,
+    btn2:false,
+    btn3:false,
+    btn4:false,
+  });
+  const setBtn =(btn)=>{
+    // console.log(btn)
+    switch (btn) {
+      case  0:
+        setActiveButton({
+          btn1:true,
+          btn2:false,
+          btn3:false,
+          btn4:false,
+        })
+       
+        break;
+        case 1:
+        setActiveButton({
+          btn1:false,
+          btn2:true,
+          btn3:false,
+          btn4:false,
+        })
+        
+        break;
+        case  2:
+        setActiveButton({
+          btn1:false,
+          btn2:false,
+          btn3:true,
+          btn4:false,
+        })
+        break;
+        case 3:
+        setActiveButton({
+          btn1:false,
+          btn2:false,
+          btn3:false,
+          btn4:true,
+        })
+        break;
+    }
+  }
   const selectBtn = (value) => {
       setSelect(value)
       console.log(select)
@@ -30,34 +74,36 @@ export const HomeOnboarding = () => {
       </View>
 
 
-    <View style={styles.contentBtnPag}>
+      <View style={styles.contentBtnPag}>
     <TouchableOpacity  style={styles.btnPag} onPress={()=> selectBtn(0)}>
       <Text>
-         <Ionicons name="radio-button-off-outline" size={15} color="black" />
+         <Ionicons name={activeButton.btn1 ? "radio-button-on-outline" : "radio-button-off-outline"} size={15} color="black" />
       </Text>
     </TouchableOpacity>
     <TouchableOpacity  style={styles.btnPag} onPress={()=> selectBtn(1)}>
       <Text>
-         <Ionicons name="radio-button-off-outline" size={15} color="black" />
+         <Ionicons  name={activeButton.btn2 ? "radio-button-on-outline" : "radio-button-off-outline"} size={15} color="black" />
       </Text>
     </TouchableOpacity>
     <TouchableOpacity  style={styles.btnPag} onPress={()=> selectBtn(2)}>
       <Text>
-      <Ionicons  name="radio-button-on-outline" size={15} color="black" />
+      <Ionicons   name={activeButton.btn3 ? "radio-button-on-outline" : "radio-button-off-outline"} size={15} color="black" />
       </Text>
     </TouchableOpacity>
     <TouchableOpacity  style={styles.btnPag} onPress={()=> selectBtn(3)}>
       <Text>
-      <Ionicons name="radio-button-on-outline" size={15} color="black" />
+      <Ionicons  name={activeButton.btn4 ? "radio-button-on-outline" : "radio-button-off-outline"} size={15} color="black" />
       </Text>
     </TouchableOpacity>
     </View>
+    
     
        
     <PagerView
      style={style.viewPager}
       initialPage={0}
       ref={ref}
+      onPageSelected={e => {setBtn(e.nativeEvent.position)}}
       >
       <View style={style.page} key="1">
          <Vista1/> 
