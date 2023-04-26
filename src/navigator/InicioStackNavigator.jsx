e2df2d611f321cedb8287c3a898c0805fe804a40
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2023-04-26 17:16:09 -0300

    Reajuste del codigo para mostrar o no el onboarding

--- src/navigator/InicioStackNavigator.jsx
+++ src/navigator/InicioStackNavigator.jsx
@@ -14,68 +14,62 @@ const Stack = createStackNavigator();
 
 
 export const InicioStackNavigator = () => {
-
-const [value, setValue] = useState(false)
 const [date, dateAction]= useContext(AuthContext) 
+  // State para saber si hay que mostrar el loading y esperar que recupere los datos del storage
+const [value, setValue] = useState({
+  isBoarding:false,
+  loading:true,
+})
 
 const onBoard = async ()=> {
   const data = await getIsBoarding(ISBOARDING)
- 
-  if (data === true && value === false) {
-    setValue(true)
-  }
+  if (data === true && value.isBoarding === false) {
+    setValue({
+      ...value,
+      isBoarding: true,
+      loading:false
+    })
+   }
 }
 
 onBoard()
+// Loading para esperar los datos
+useEffect(() => {
+  const timeoutId = setTimeout(() => {
+    setValue({
+      ...value,
+      loading:false
+    })
+  }, 3000);
+
+  return () => {
+    clearTimeout(timeoutId);
+  };
+}, []);
+
 
 // Pagina de carga
- if (date.status === 'checking') {
+ if (date.status === 'checking' || value.loading === true) {
    return <LoadingScreen/>
  }
 
   return (
     <Stack.Navigator 
-       initialRouteName={ value === true ? "HomeRegistrarIniciarSesion"  : 'HomeOnboarding'}
+       initialRouteName={ value.isBoarding === true ? "HomeRegistrarIniciarSesion"  : 'HomeOnboarding'}
       options={{
         headerStyle:{
           backgroundColor:'#16253A',
         }
       }}
     >
-       {/* {value === false ?  (
-       <>
+       {
+         value.isBoarding === false &&
          <Stack.Screen name="HomeOnboarding"  options={{
           title:'Login',
          headerShown:false
         }}component={HomeOnboarding} /> 
-      
-      <Stack.Screen name="HomeRegistrarIniciarSesion" 
-      options={{
-          title:'Login',
-         headerShown:false
-        }} 
-      component={HomeRegistrarIniciarSesion} />
-      <Stack.Screen name="InicioSesion" options={{title:'Iniciar Sesion'}} component={InicioSesion} />
-      <Stack.Screen name="CrearCuenta" options={{title:'Crear Cuenta'}} component={CrearCuenta} />
-       </>
-       ):(
-       <>
-    
-      <Stack.Screen name="HomeRegistrarIniciarSesion" 
-      options={{
-          title:'Login',
-         headerShown:false
-        }} 
-      component={HomeRegistrarIniciarSesion} />
-      <Stack.Screen name="InicioSesion" options={{title:'Iniciar Sesion'}} component={InicioSesion} />
-      <Stack.Screen name="CrearCuenta" options={{title:'Crear Cuenta'}} component={CrearCuenta} />
-       </>
-       ) 
-        */}
+       }
        
-    
-     
-{/*        
      
     <Stack.Screen name="HomeRegistrarIniciarSesion" 
     options={{
@@ -86,25 +80,7 @@ onBoard()
     <Stack.Screen name="InicioSesion" options={{title:'Iniciar Sesion'}} component={InicioSesion} />
     <Stack.Screen name="CrearCuenta" options={{title:'Crear Cuenta'}} component={CrearCuenta} />
       
-     */
      
-     !value &&
-      <Stack.Screen name="HomeOnboarding"  options={{
-          title:'Login',
-         headerShown:false
-        }}component={HomeOnboarding} /> 
-     }
-    
-    
-      
-      <Stack.Screen name="HomeRegistrarIniciarSesion" 
-      options={{
-          title:'Login',
-         headerShown:false
-        }} 
-      component={HomeRegistrarIniciarSesion} />
-      <Stack.Screen name="InicioSesion" options={{title:'Iniciar Sesion'}} component={InicioSesion} />
-      <Stack.Screen name="CrearCuenta" options={{title:'Crear Cuenta'}} component={CrearCuenta} />
     </Stack.Navigator>
    
   );
