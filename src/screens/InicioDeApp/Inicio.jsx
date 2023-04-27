7b549b8d09e6c4986be8618e1fb14f23d23e0218
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2023-04-27 11:35:04 -0300

    No cerrar la app si no se cierra la sesion

--- src/screens/InicioDeApp/Inicio.jsx
+++ src/screens/InicioDeApp/Inicio.jsx
@@ -6,15 +6,62 @@ import { MenuLateral } from '../../navigator/MenuLateral';
 import { InicioStackNavigator } from '../../navigator/InicioStackNavigator';
 import { AuthContext } from '../../contextCrearUsuario/AuthContext';
 import { useContext } from 'react';
+import { getUserToken } from '../../helpers/store';
+import { TOKEN } from '../../helpers/const';
+import { LoadingScreen } from '../Loading/LoadingScreen';
 
 
 export default function Incio() {  
  const [date, dateAction] = useContext(AuthContext);  
+   // State para saber si hay que mostrar el loading y esperar que recupere los datos del storage
+const [value, setValue] = useState({
+  token:false,
+  loading:true,
+})
 
+const getAction = async ()=> {
+  const data = await getUserToken(TOKEN)
+  
+  if (data && value.token === false) {
+    console.log(data, 'entra:?')
+    setValue({
+      ...value,
+      token: true,
+      loading:false
+    })
+   }else if(!data && value.token === true){
+    setValue({
+      ...value,
+      token: false,
+      loading:false
+    })
+   }
+ 
+}
+
+getAction()
+// Loading para esperar los datos
+useEffect(() => {
+  const timeoutId = setTimeout(() => {
+    setValue({
+      ...value,
+      loading:false
+    })
+  }, 4000);
+
+  return () => {
+    clearTimeout(timeoutId);
+  };
+}, []);
+
+if (value.loading === true) {
+  return <LoadingScreen/>
+}
+console.log(value.token)
   return (
     <NavigationContainer>  
     
-       {date.status === "not-authenticated" ?  (
+       {value.token === false?  (
            <InicioStackNavigator/>
         )
         : ( 
