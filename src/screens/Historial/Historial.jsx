94e0e4a9b83f8df50d2578199a46587c4226b8bf
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2023-04-28 14:04:29 -0300

    ESTILOS DEL HISTORIAL

--- src/screens/Historial/Historial.jsx
+++ src/screens/Historial/Historial.jsx
@@ -6,6 +6,10 @@ export const Historial = () => {
   return (
     <View>
        <ScrollView>
+       <View style={style.contentFiltro}>
+        <Text style={style.title}>ÚLTIMO MES DE ACTIVIDAD</Text>
+        <Text style={style.filtro}>Última fecha</Text>
+       </View>
         <HistoryData/>
        </ScrollView>
       
@@ -13,8 +17,19 @@ export const Historial = () => {
   )
 }
 const style = StyleSheet.create({
-  contentHistorial:{
-     flex:1,
-     alignItems:'center',
+  // contentHistorial:{
+  //    flex:1,
+  // },
+  contentFiltro:{
+    alignSelf:'center',
+    width:'90%',
+    flexDirection:'row',
+    height:80,
+    justifyContent:'space-between',
+    alignItems:'center'
+  },
+  title:{
+    fontSize:16,
+    color:'#49454F',
   }
 })
